085ddc0f6435db704c998119b560c2e85524b97b
mkuhler <madison.kuhler@gmail.com>
2025-11-24 23:59:17 +0000

    update test with separation logic annotations

--- demo.c
+++ demo.c
@@ -1,26 +1,60 @@
 #include <stdlib.h>
 
-/* HOW TO RUN TESTS:
-   infer run -- gcc -c demo.c
+/* 
+* HOW TO RUN TESTS:
+* infer run -- gcc -c demo.c
 */
 
-void test_null_dereference() {
+/* 
+* TEST 1: NULL DEREFERENCE
+* In this example, a pointer is being
+* accessed before it is assigned in memory
+* if the condition x <= 0 is met.
+* The code is annotated with Infer
+* separation logic triples
+*/
+int* make_unsafe_ptr(int x) {
+  // precondition: {true}
+  int* ptr = NULL;
+  // postcondition: {ptr -> null}
+  
   /*
-  TEST 1: NULL DEREFERENCE
-  In this example, a pointer is being
-  accessed before it is assigned in memory.
+  * branch: x <= 0
+  * precondition: {x <= 0 ∧ ptr -> null}
+  * postcondition: {result -> null}
   */
-  // int* ptr = NULL;
-  // *ptr = 42;
 
-  /*
-  TEST 1: FIX
-  To resolve this error, we simply insert
-  a null pointer check. Infer will now report
-  no issues found!
+  // branch: x > 0
+  if (x > 0) {
+    // precondition: {x > 0 ∧ ptr -> null}
+    ptr = malloc(sizeof(int));
+    // postcondition: {ptr -> ?} // here ? means allocated but uninitialized
+
+    // precondition: {ptr -> ?}
+    if (ptr != NULL) { *ptr = x; }
+    // {ptr -> x ∨ ptr -> null} // ptr points to x if allocation succeeded, or null if failed
+  } 
+
+  // postcondition: {result -> x ∨ result -> null}
+  return ptr;
+}
+
+void test_null_dereference() {
+  int x = 42;
+  int* ptr = make_unsafe_ptr(x);
+  *ptr = x;
+
+  /* 
+  * FIX 1: NULL DEREFERENCE
+  * since make_unsafe_ptr can return a null pointer,
+  * we must check if n = NULL 
   */
-  // int* ptr_fix = NULL;
-  // if (ptr_fix != NULL) {
-  //   *ptr_fix = 42;
-  // }
+  //if (ptr != NULL) { *ptr = x; }
+
+  /* 
+  * FIX 2: MEMORY LEAK
+  * We dynamically allocate n with malloc,
+  * but this memory is not freed after use.
+  */
+  //free(ptr);
 }
