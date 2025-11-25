483a101a30b12b08f16dc88c475fdb74dbdb4877
mkuhler <madison.kuhler@gmail.com>
2025-11-25 05:52:07 +0000

    add comment notes

--- demo_datarace.java
+++ demo_datarace.java
@@ -1,11 +1,30 @@
 import com.facebook.infer.annotation.ThreadSafe;
+/* 
+* HOW TO RUN TESTS:
+* infer --racerd-only -- javac -cp lib/infer-annotation-0.18.0.jar -d build demo_datarace.java
+* infer --racerd -- javac -cp lib/infer-annotation-0.18.0.jar -d build demo_datarace.java
+* infer --no-racerd -- javac -cp lib/infer-annotation-0.18.0.jar -d build demo_datarace.java
+*/
 
+/*
+* ANNOTATIONS
+* Annotations in Infer specify properties to their compiler
+* in this case, the ThreadSafe annotation tells Infer that
+* this class needs to be thread safe, otherwise exception.
+*/
 @ThreadSafe
 public class demo_datarace {
   private int mUnsafeVar;
 
   public void testThreadSafetyViolation() {
+  /* 
+  * FIX: THREAD SAFETY VIOLATION
+  * The synchronized keyword in Java resolves this issue and
+  * achieves thread safety. It enforces mutual exclusion - 
+  * only one thread can execute a synchronized block at a time.
+  */
   //public synchronized void testThreadSafetyViolation() {
+
     setUnsafeVar();
   }
 
